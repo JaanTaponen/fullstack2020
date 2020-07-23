fd967c70c80d95672b83a96f1f86ff19889860b5
JaanTaponen <jaan.taponen@hotmail.com>
2020-07-23 18:02:34 +0300

    part4 start

---
+++ part4/blogs/models/blog.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+module.exports = mongoose.model('Blog', blogSchema)
