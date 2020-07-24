3fedb5ef8dde09558dde0f1607c2728afdd737d4
JaanTaponen <jaan.taponen@hotmail.com>
2020-07-24 11:58:02 +0300

    part 3b

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,20 +1,40 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+require('express-async-errors')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-    blog.save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+blogsRouter.post('/', async (request, response) => {
+    const blog = await new Blog(request.body).save()
+    response.status(201).json(blog).end()
+})
+
+
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    response.json(blog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    const newBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    response.json(newBlog)
 })
 
 module.exports = blogsRouter
--- part4/blogs/models/blog.js
+++ part4/blogs/models/blog.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 const config = require('../utils/config')
+mongoose.set('useFindAndModify', false);
 
 const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+    title: { type: String, required: true }, 
+    author: { type: String, required: true }, 
+    url: { type: String, required: true }, 
+    likes: { type: Number, required: false, default: 0 }, 
 })
 
 blogSchema.set('toJSON', {
